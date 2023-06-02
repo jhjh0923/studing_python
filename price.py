49889116a67be78920eaf9eadc415d8aa8c1007b
jhjh0923 <135347669+jhjh0923@users.noreply.github.com>
2023-06-03 00:34:41 +0900

    price.py

--- price.py
+++ price.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
+data_path = Path(__file__).resolve().parent / "data"
+data_path.mkdir(parents=True, exist_ok=True)
+
 def shopping(shop_file):
-    data_path = Path() / "data"
-    data_path.mkdir(parents=True, exist_ok=True)
     shop_dict = {}
     with open(data_path / shop_file, 'r') as f:
       next(f)
