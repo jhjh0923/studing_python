fbc2d032d31d2ba8a91084c09d182695b645a9d9
jhjh0923 <135347669+jhjh0923@users.noreply.github.com>
2023-06-02 23:45:38 +0900

    Add files via upload

---
+++ price.py
@@ -0,0 +1,16 @@
+def shopping(shop_file):
+    shop_dict = {}
+    with open(data_path / shop_file, 'r') as f:
+     next(f)
+     for line in f:
+        elements = line.strip().split()
+        if len(elements) == 2:
+            name, price = elements
+            shop_dict[name] = price
+
+    return shop_dict
+
+def item_price(shop_file, item):
+  price_dict = shopping(shop_file)
+
+  return price_dict[item]
