b52f701d118de31fd7a480586f4bcd73dbe32f25
jhjh0923 <135347669+jhjh0923@users.noreply.github.com>
2023-06-03 02:42:58 +0900

    price.py

--- price.py
+++ price.py
@@ -1,18 +1,14 @@
 from pathlib import Path
 
-content_path = Path('/') / 'content'
-data_path = content_path / "data"
-data_path.mkdir(parents=True, exist_ok=True)
-
-def shopping(shop_file):
+def shopping(shop_file, data_path):
     shop_dict = {}
     with open(data_path / shop_file, 'r') as f:
-      next(f)
-      for line in f:
-        elements = line.strip().split()
-        if len(elements) == 2:
-            name, price = elements
-            shop_dict[name] = price
+        next(f)
+        for line in f:
+            elements = line.strip().split()
+            if len(elements) == 2:
+                name, price = elements
+                shop_dict[name] = price
 
     return shop_dict
 
